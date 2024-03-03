20ca3d8de3902c717dbc2800fdda81e3d0ee9320
GimenezCristian <gimenezc779@gmail.com>
2024-03-03 02:57:24 -0300

    Personalizacion de la pantalla de carga

--- app/src/main/java/com/cristiangimenez/fondografia/Carga.java
+++ app/src/main/java/com/cristiangimenez/fondografia/Carga.java
@@ -3,15 +3,30 @@ package com.cristiangimenez.fondografia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.TextView;
 
 public class Carga extends AppCompatActivity {
+
+    TextView app_name, desarrollador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.carga);
+
+
+        //CAMBIO DE LETRA
+        String ubicacion = "fuentes/sans_negrita.ttf";
+        Typeface tf = Typeface.createFromAsset(Carga.this.getAssets(),ubicacion);
+        //CAMBIO DE LETRA
+
+        app_name = findViewById(R.id.app_name);
+        desarrollador = findViewById(R.id.desarrollador);
+
+
 
         final int DURACION = 3000;
 
@@ -26,5 +41,8 @@ public class Carga extends AppCompatActivity {
 
             }
         },DURACION);
+
+        app_name.setTypeface(tf);
+        desarrollador.setTypeface(tf);
     }
 }
